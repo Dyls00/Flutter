abcfd09663f5691a7ea45f7c9f246a68566e7e65
johane <nsangouakandajohane@gmail.com>
2024-11-28 02:07:39 +0100

    push

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,21 +8,56 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 78, 49, 49),
+      backgroundColor: Color(0xFF5F67EA),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 700,
-              color: Color.fromARGB(255, 197, 34, 34),
+          child: Stack(
+        children: [
+          Transform(
+            transform: Matrix4.identity()..rotateZ(20),
+            origin: const Offset(150, 50),
+            child: Image.asset('assets/images/bg_liquid.png', width: 200),
+          ),
+          Positioned(
+            right: 0,
+            top: 200,
+            child: Transform(
+              transform: Matrix4.identity()..rotateZ(20),
+              origin: const Offset(150, 100),
+              child: Image.asset('assets/images/bg_liquid.png', width: 200),
             ),
-            Container(
-              height: 700,
-              color: Color.fromARGB(255, 218, 90, 90),
-            ),
-          ],
+          ),
+          Container(),
+        ],
+      )),
+      bottomNavigationBar: NavigationBar(),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget NavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          label: 'Accueil',
+          icon: Icon(Icons.home),
         ),
-      ),
+        BottomNavigationBarItem(
+          label: 'Alevins',
+          icon: Icon(
+            Icons.set_meal,
+            size: 30,
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: 'Aliments',
+          icon: Icon(Icons.takeout_dining),
+        ),
+        BottomNavigationBarItem(
+          label: 'Vente',
+          icon: Icon(Icons.shopping_cart),
+        ),
+      ],
     );
   }
 }
